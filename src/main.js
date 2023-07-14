c4932ac26c8bde285dbcdb0a5c1f746be5934ee6
VamsiMutyala98 <vamshi.mutyala@fissionlabs.com>
2023-07-14 22:01:08 +0530

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import Notifications from '@kyvg/vue3-notification'
+import Dropdown from 'v-dropdown'
+import './utils/style';
 
-createApp(App).use(store).use(router).mount('#app')
+window.$ = require('jquery');
+
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.use(Notifications);
+app.use(Dropdown);
+app.mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,77 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginPage from '../components/auth/Login.vue';
+import SignUpPage from '../components/auth/Signup.vue';
+import ForgetPasswordPage from '../components/auth/ForgetPassword.vue';
+import HomePage from '../components/home/Home.vue';
+import ProductsPage from '../components/products/Product.vue';
+import CartPage from '../components/products/Cart.vue';
+import PageNotFound from '@/components/widgets/PageNotFound.vue';
+import CheckOutPage from '@/components/widgets/CheckOut.vue';
+import { getDetailsFromLocalStorage } from '@/utils/helper';
+
+export function isAuthenticated() {
+  const accessToken = getDetailsFromLocalStorage('access-token');
+  return !!accessToken;
+}
+
+export function requireAuth(to, from, next) {
+  if (!isAuthenticated() && to.name !== 'Login') {
+    next('/login');
+  } else if (to.name === 'Login' && isAuthenticated()) {
+    next(from.fullPath);
+  } else {
+    next();
+  }
+}
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/login',
+    name: 'Login',
+    component: LoginPage,
+    beforeEnter: requireAuth,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUpPage,
+  },
+  {
+    path: '/forget-password',
+    name: 'ForgetPassword',
+    component: ForgetPasswordPage,
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: HomePage,
+    beforeEnter: requireAuth,
+    children: [
+      {
+        path: '/',
+        name: 'Products',
+        component: ProductsPage,
+        beforeEnter: requireAuth,
+      },
+      {
+        path: '/cart-items',
+        name: 'CartItems',
+        component: CartPage,
+        beforeEnter: requireAuth,
+      },
+      {
+        path: 'checkout-page',
+        name: 'CheckOut',
+        component: CheckOutPage,
+        beforeEnter: requireAuth,
+      }
+    ]
+  },
+  {
+    path: '/:catchAll(.*)',
+    component: PageNotFound,
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
